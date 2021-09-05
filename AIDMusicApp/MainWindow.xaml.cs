45ecee4ea664aa405c7e7f5f57e969928a55cbf2
Distaid <88453698+Distaid@users.noreply.github.com>
2021-09-05 19:32:50 +0300

    Добавлена панель загрузки

--- AIDMusicApp/MainWindow.xaml.cs
+++ AIDMusicApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AIDMusicApp.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,7 @@ namespace AIDMusicApp
         {
             InitializeComponent();
             MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MainContent.Content = new LoadingControl();
 
             TitleBar.MouseDown += TitleBar_MouseDown;
             TitleHideButton.Click += TitleHideButton_Click;
