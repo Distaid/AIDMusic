be6dcd49f5614ef261fe526de24a2f7f79efaad9
Distaid <88453698+Distaid@users.noreply.github.com>
2021-09-08 20:40:30 +0300

    Создано окно сообщения

---
+++ AIDMusicApp/Windows/MessageWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AIDMusicApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow(string message)
+        {
+            InitializeComponent();
+
+            TitleText.Text = "";
+            MessageText.Text = message;
+
+            MouseDown += MessageWindow_MouseDown;
+        }
+
+        public MessageWindow(string header, string message)
+        {
+            InitializeComponent();
+
+            TitleText.Text = header;
+            MessageText.Text = message;
+
+            MouseDown += MessageWindow_MouseDown;
+        }
+
+        private void MessageWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
