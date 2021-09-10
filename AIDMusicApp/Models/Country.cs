3e7917b1912a5b0e12a77bd70ee9865e186806df
Distaid <88453698+Distaid@users.noreply.github.com>
2021-09-10 18:01:39 +0300

    Добален метод копирования

--- AIDMusicApp/Models/Country.cs
+++ AIDMusicApp/Models/Country.cs
@@ -5,5 +5,14 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public Country Copy()
+        {
+            return new Country
+            {
+                Id = Id,
+                Name = Name
+            };
+        }
     }
 }
--- AIDMusicApp/Models/User.cs
+++ AIDMusicApp/Models/User.cs
@@ -15,5 +15,21 @@
         public int AccessId { get; set; }
 
         public byte[] Avatar { get; set; }
+
+        public User Copy()
+        {
+            var user = new User
+            {
+                Id = Id,
+                Login = Login,
+                Password = Password,
+                Phone = Phone,
+                Email = Email,
+                AccessId = AccessId
+            };
+            Avatar.CopyTo(user.Avatar, 0);
+
+            return user;
+        }
     }
 }
