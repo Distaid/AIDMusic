77644d04304064476621ac28c5105865866e2140
Distaid <88453698+Distaid@users.noreply.github.com>
2021-10-27 21:44:03 +0300

    Переопределен метод ToString и добавлен метод копирования

--- AIDMusicApp/Models/Access.cs
+++ AIDMusicApp/Models/Access.cs
@@ -5,5 +5,19 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public Access Copy()
+        {
+            return new Access
+            {
+                Id = Id,
+                Name = Name
+            };
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
